edb712327b0196041ffdbbc7cbfe266765f33d13
DKrKostadinov22 <DKrKostadinov22@codingburgas.bg>
2025-03-05 22:34:02 +0200

    Feat program flow

---
+++ ConsoleApplication1/Menu.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Admin
+{
+public:
+	void adminMenu();
+};
+
+class Customer
+{
+public:
+	void customerMenu();
+};
+
+void clearscreen();
+void menuChoice();
